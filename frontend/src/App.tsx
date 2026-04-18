4c8b9cb034d8e4dbb2f0f8143dba437018c3b5bd
maikolmontes <montenegromaikol4@gmail.com>
2026-04-18 12:52:13 -0500

    feat: configurar Tailwind CSS en frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        SIGAP funcionando con Tailwind
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
