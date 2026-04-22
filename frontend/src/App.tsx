7d7d36cf2fe4736ec30ea3515bf210a77a978170
maikolmontes <montenegromaikol4@gmail.com>
2026-04-21 21:40:12 -0500

    fix: recuperar modulo periodos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import { ProtectedRoute } from './components/common/ProtectedRoute'
 import Login from './pages/auth/Login'
 import DashboardPlaneacion from './pages/planeacion/DashboardPlaneacion'
 import Docentes from './pages/planeacion/Docentes'
+import Periodos from './pages/planeacion/Periodos'
 import DashboardDirector from './pages/director/DashboardDirector'
 import Perfil from './pages/common/Perfil'
 import Configuracion from './pages/common/Configuracion'
-
 
 function App() {
   return (
@@ -19,11 +19,11 @@ function App() {
         {/* Planeación */}
         <Route path="/planeacion/dashboard" element={<DashboardPlaneacion />} />
         <Route path="/planeacion/docentes" element={<Docentes />} />
-        <Route path="/planeacion/periodos" element={<h1>PERIODOS FUNCIONANDO</h1>} />
-
+        <Route path="/planeacion/periodos" element={<Periodos />} />
+        
         {/* Director */}
         <Route path="/director/dashboard" element={<DashboardDirector />} />
-
+        
         {/* Comunes (Independiente del Rol principal) */}
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/configuracion" element={<Configuracion />} />
