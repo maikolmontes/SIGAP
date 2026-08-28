7e5dd0828b14af16360ff74ba063bc277f7a9bac
JeissonSantander <160243289+JeissonSantander@users.noreply.github.com>
2026-08-28 11:52:16 -0500

    cambiar a vercel

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,4 @@ app.get('/api', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+module.exports = app;
